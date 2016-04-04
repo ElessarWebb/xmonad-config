0ef69073477b0bfb577f4629ec9c84b837b318f8
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2016-04-04 12:56:05 +0200

    Minor improvements

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 
+
 import Data.Ratio
 import Data.Maybe
 import Data.Monoid
 import Data.Map as Map
 
 import XMonad
+import XMonad.Config.Xfce
 import XMonad.Util.EZConfig
 import XMonad.Actions.CycleWS
 import XMonad.Actions.GridSelect
@@ -22,8 +24,14 @@ import XMonad.Layout.MultiToggle.Instances
 import XMonad.Layout.Spacing
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.Tabbed
+import XMonad.Layout.Grid
+import XMonad.Layout.Gaps
+import XMonad.Layout.ResizableTile
+import qualified XMonad.Layout.Magnifier as Mag
+import XMonad.Layout.Magnifier hiding (Toggle)
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.EwmhDesktops hiding (fullscreenEventHook)
 import XMonad.Actions.GridSelect
 import XMonad.Actions.WindowBringer
 import System.IO
@@ -31,9 +39,8 @@ import System.IO
 import qualified XMonad.StackSet as W
 import qualified XMonad.Util.ExtensibleState as XS
 
--- workspaces: named from 0 to 9
-myWorkspaces = [ "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" ]
-
+-- workspaces: named from 0 to 10
+myWorkspaces = [ "SH", "Web", "Code", "PDF", "Chat", "VI", "VII", "VIII", "Music" ]
 
 
 -- Colors for text and backgrounds of each tab when in "Tabbed" layout.
@@ -46,28 +53,28 @@ tabconfig = defaultTheme {
   inactiveColor = "#002b36"
 }
 
-layout = id
+layout = gaps [(U, 35), (R, 0)]
         . smartBorders
-        . smartSpacing 0
+        . smartSpacing 5
         . mkToggle (NOBORDERS ?? FULL ?? EOT)
-        $ tiled ||| tabbed shrinkText tabconfig |||  Full
-          where tiled = Tall 1 (3/100) (1/2)
+        $ tiled ||| Grid ||| tabbed shrinkText tabconfig ||| Full
+          where tiled = ResizableTall 1 (3/100) (2/3) []
 
 -- Forward the window information to the left dzen bar and format it
 myLogHook h = dynamicLogWithPP $ myDzenPP { ppOutput = hPutStrLn h }
 
-myDzenStatus = "dzen2 -x '0' -w '750' -ta 'l'" ++ " -h '20' -y '0' -fg '#eeeeee' -bg '#002b36'"
+myDzenStatus = "dzen2 -x '0' -w '750' -ta 'l'" ++ " -h '25' -y '1055' -fg '#eeeeee' -bg '#002b36'"
 
 -- Very plain formatting, non-empty workspaces are highlighted,
 -- urgent workspaces (e.g. active IM window) are highlighted in red
 myDzenPP  = dzenPP
-    { ppCurrent = wrap (c "#ffffff" " [ ") (c "#ffffff" " ] ") . (c "#268bd2")
-    , ppHidden  = c "#dddddd" . wrap " " " "
+    { ppCurrent = wrap "  " "  " . (c "#8FC0CC")
+    , ppHidden  = c "#1E6E82" . wrap "  " "  "
     , ppHiddenNoWindows = \y -> ""
-    , ppUrgent  = c "#ff0000" . wrap " " " "
+    , ppUrgent  = c "#8FC0CC" . wrap "  " "  "
     , ppSep     = "   |   "
-    , ppLayout  = \y -> ""
-    , ppTitle   = wrap (c "#ffffff" " [ ") (c "#ffffff" " ] ") . (c "#859900")
+    , ppLayout  = const ""
+    , ppTitle   = const ""
     }
     where c = flip dzenColor ""
 
@@ -76,18 +83,20 @@ toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b )
 main = do
 
   -- spawn conky
-  conky <- spawnPipe "conky -c ~/.xmonad/conkyrc"
+  -- conky <- spawnPipe "conky -c ~/.xmonad/conkyrc"
 
   -- spawn dzen
-  leftbar <- statusBar myDzenStatus myDzenPP toggleStrutsKey myconfig
+  -- leftbar <- statusBar myDzenStatus myDzenPP 
 
   -- hopla
-  xmonad leftbar
+  xmonad $ ewmh myconfig
 
 myconfig = defaultConfig {
     modMask = mod4Mask,
-    borderWidth = 0,
-    layoutHook = layout,
+    borderWidth = 1,
+    normalBorderColor = "#002b36",
+    focusedBorderColor = "#2A788C",
+    layoutHook = avoidStruts layout,
     workspaces = myWorkspaces,
     handleEventHook = fullscreenEventHook <+> docksEventHook,
     manageHook = fullscreenManageHook <+> (isFullscreen --> doFullFloat) <+> manageDocks,
@@ -97,13 +106,21 @@ myconfig = defaultConfig {
   `additionalKeys`
 
   ([
-    -- client management
+    -- m-s-h to swap to master
       ((mod4Mask .|. shiftMask, xK_h), windows W.swapMaster)
-    , ((mod4Mask, xK_f), (sendMessage $ Toggle FULL) >>= (\x -> sendMessage $ ToggleStrut U))
+
+    -- m-f full screen toggles strut
+    , ((mod4Mask, xK_f), (sendMessage $ Toggle FULL) >>= (\_ -> sendMessage $ ToggleStrut U))
+    -- m-b toggles strut
     , ((mod4Mask, xK_b), (sendMessage $ ToggleStrut U))
 
+    -- c-m to toggle magnification
+    , ((mod4Mask .|. controlMask, xK_m), (sendMessage Mag.Toggle ))
+    , ((mod4Mask,               xK_z), sendMessage MirrorShrink)
+    , ((mod4Mask,               xK_a), sendMessage MirrorExpand)
     -- utility
     , ((mod4Mask .|. shiftMask, xK_r), spawn "xmonad --recompile")
+    , ((mod4Mask .|. shiftMask, xK_q), spawn "xfce4-session-logout")
     , ((mod4Mask, xK_g), goToSelected defaultGSConfig)
     , ((mod4Mask .|. shiftMask, xK_g), gotoMenu)
     , ((mod4Mask .|. shiftMask, xK_b), bringMenu)
